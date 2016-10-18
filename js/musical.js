711582a6210c4cba0eb01b8d5021657e38f31338
michaelslevy <u41745915@cbi-bs1.server.lan>
2016-10-18 10:56:38 -0400

    FIX: Clean up playback and improve attack and decay.

--- js/musical.js
+++ js/musical.js
@@ -10,6 +10,7 @@ var Musical = function(){
     self.durationMicroSeconds=self.duration*1000
     self.playPosition=0;
     self.playTimer=false;    
+    var terminal=true; //determines whether tone ends after note is played
     
     var audioContext = new(AudioContext || webkitAudioContext)();
 
@@ -28,21 +29,22 @@ var Musical = function(){
     gainNode.gain.value = 0;
     self.oscillator.start(0);
     
-
     self.playTone =function() {
-    
+    	
       var now = audioContext.currentTime;
       self.setCurrentPitch();      
       self.oscillator.frequency.linearRampToValueAtTime(self.currentPitch,now);
       gainNode.gain.linearRampToValueAtTime(.2,now+.0008);
-      // The sound should last for 250ms
+      
+      // end note if terminal
       setTimeout(function() {
         var now = audioContext.currentTime;
-        gainNode.gain.linearRampToValueAtTime(0,now+.0008);
-          
-        self.oscillator.frequency.setValueAtTime(self.currentPitch, now+.0008);
-
-      }, 300);
+       	
+       	if(terminal==true){
+       		gainNode.gain.linearRampToValueAtTime(0,now+self.duration);
+       	}
+       	 
+      }, self.durationMicroSeconds*1.5);
      
     }
     
@@ -55,23 +57,33 @@ var Musical = function(){
         var ratio=parseFloat(self.baseLineLength/self.currentLineLength);
         /* apply ratio to base pitch and set the current pitch */
         self.currentPitch=self.basePitch*ratio;
-        console.log(self.baseLineLength,self.currentLineLength,"ratio:"+ratio);
         
     }
     
+
+    
      /* play music */
     self.play=function(){
-        
+         terminal=false;
         self.playTimer=setInterval(function(){ playNext() }, self.durationMicroSeconds);
         
         return self;
     }  
     
     var playNext=function(){
+    	
+     
      
         var toneLine=$("#musicallines line").eq(self.playPosition);
         $("#musicallines line").removeClass('playing');
         toneLine.addClass('playing');
+        
+        /* If last note, indicate to stop oscilator */
+        if(self.playPosition>($("#musicallines line").length-2)){
+        	terminal=true;	
+        }	
+        
+        /* end playback if position greater than number of lines. */
         if(self.playPosition>($("#musicallines line").length-1)){
             clearInterval(self.playTimer);
             self.playPosition=0;
