0f4ab85dc57eb6898d2570ade54d070dc94edd8f
michaelslevy <u41745915@cbi-bs1.server.lan>
2016-10-20 19:15:53 -0400

    FEAT: Seperate intersection clicks into its own class

--- js/draw.js
+++ js/draw.js
@@ -6,28 +6,13 @@
 var DrawApp = function(){
 	  
 	self=this;
-	coordDictionary=new CoordDictionary();  
-	  
-	 /* CLICK EVENTS
-	  * Click events are either starting points or ending points 
-	  * clicknum keeps track of which stage you are at. 
-	  * 1. start
-	  * 2. end
-	  */
-	 
-	var clicknum=0; 
-	var mode="line";
-    
     var musicPlayer=new Musical();
     var windowZoom=new WindowZoom();
     var windowPan=new WindowPan();
+    var nodeClicks=new NodeClicks();
     
-	var newLineCoord = {
-        x1: "nan",
-        y1: "nan",
-        x2: "nan",
-        y2: "nan"
-    }
+    var mode="line";
+    setMode("line");
     
     self.init=function(){
     	setUpCanvas();
@@ -38,7 +23,10 @@ var DrawApp = function(){
     
     $(window).resize(function(){updateWindow()});
     
-    //Register Keyboard Events
+      /*****************************************
+      ******** Register Key Up Events**********
+      ****************************************/   
+
    $(document).keyup(function(e) {
 	  e = e || window.event; 
        
@@ -50,7 +38,7 @@ var DrawApp = function(){
 		switch (keycode){
             //esc    
 			case 27:
-				reset_vars();
+				nodeClicks.reset_vars();
 			break;
                
               //space  
@@ -60,7 +48,7 @@ var DrawApp = function(){
 			break;
                 
             case 69:
-                mode="erase";
+                setMode("erase");
                 $("#toolbox button").removeClass("active");
                 $("#eraseMode").addClass("active");
 				updateNestClass();
@@ -68,7 +56,7 @@ var DrawApp = function(){
             
             //(l)ine
             case 76:
-                mode="line";
+                setMode("line");
                  $("#toolbox button").removeClass("active");
                 $("#lineMode").addClass("active");
             break; 
@@ -80,7 +68,7 @@ var DrawApp = function(){
                 
             //(m)usical   
             case 77:
-                mode="musical";
+                setMode("musical");
                  $("#toolbox button").removeClass("active");
                 $("#musical").addClass("active");
 			break;    
@@ -110,6 +98,9 @@ var DrawApp = function(){
 
 	});
         
+     /*****************************************
+      ********** Register Key Down************
+      ****************************************/   
     $(document).keydown(function(event) {
         
       //  alert(event.which);
@@ -136,231 +127,59 @@ var DrawApp = function(){
        }       
         
     });
-	 
-	$(document).on('click',".intersection",function(){
-		
-  		var my_x=this.getAttribute("cx");
-		var my_y=this.getAttribute("cy");
-				
-		/* get mouse positions */
-				
-		clicknum++;
-		
-		if(clicknum == 1){
-			//starting coordinates
-			newLineCoord.x1=my_x;
-			newLineCoord.y1=my_y;
-						
-		} else if (clicknum == 2){ 
-			
-			//ending coordinates
-			newLineCoord.x2=my_x;
-			newLineCoord.y2=my_y;
-            
-            $("line.preview_line").attr({"x2":my_x, "y2":my_y});
-            
-			var current_line; 
-						
-			switch (mode){
-				
-				case "line":
-					//add new line
-					add_line('#guidelines');
-					var current_line=$(".guideline").last(); 
-
-				break;
-                    
-                case "musical":
-				//add new line
-				    add_line("#musicallines");
-				    var current_line=$("#musicallines line").last(); 
-                    musicPlayer.playPreviewTone();
-				break;
-				
-				case "circle-center":
-					add_circle("guide");
-					current_line=$(".guide").last(); 
-				break;
-				
-				case "circle-edge":
-					add_circle("guide");
-					current_line=$(".guide").last(); 
-				break;
-				
-			}
-            
-			coordDictionary.currentElement=current_line; 
-			coordDictionary.find_coords(); 
-		
-			//remove the preview line
-			reset_vars();
-		
-		}		
-	});
 	
-	/* MOUSE MOVE */
+	  /*******************************************
+      ********** Register Mouse Move ************
+      ********************************************/   
 	document.onmousemove = function(e) { 
         
         windowPan.checkPan(e);
-        
         if(isDrawMode()==true){
             //draw preview line
-
-            if(newLineCoord.x1!='nan' ) {
-
-                var panOffset=windowPan.getPanOffset();
-
-                $('.preview_line').remove();
-                var mouseCoords=windowZoom.scaleMouseCoords(e);
-                newLineCoord.x2=mouseCoords["x"]+panOffset["x"];
-                newLineCoord.y2=mouseCoords["y"]+panOffset["y"];
-
-                jQuery('#preview').Guideline({css_class:"preview_line",x1: newLineCoord.x1, y1:newLineCoord.y1, x2:newLineCoord.x2, y2:newLineCoord.y2}).draw();
-                if(mode=="circle-center" || mode=="circle-edge"){
-                    add_circle("preview_line");
-                }
-                
-                 var lineWidth= windowZoom.getLineWidth();
-                $("#nest #preview line").css("stroke-width",lineWidth);
-                $("#nest #preview circle").css("stroke-width",lineWidth);
-
-            } 
+			 var panOffset=windowPan.getPanOffset();
+			 var mouseCoords=windowZoom.scaleMouseCoords(e);
+			nodeClicks.drawPreviewLine(panOffset, mouseCoords, mode);
         }    
         
 	}
-	
-	function reset_vars(){ 
-		clicknum=0;
-		newLineCoord.x1="nan";
-		newLineCoord.y1="nan";
-		newLineCoord.x2="nan";
-		newLineCoord.y2="nan";
-		$('.preview_line').remove();
-        windowZoom.updateZoomDimension();
-	}
-	
-	function add_line(id){
-		x1v=newLineCoord.x1;
-		y1v=newLineCoord.y1;
-		x2v=newLineCoord.x2;
-		y2v=newLineCoord.y2;
-		
-		jQuery(id).Guideline({x1: x1v, y1:y1v, x2:x2v, y2:y2v}).draw();
-		 
-	}
-	
-	function add_circle(className){
-		//define variables
-		var myClass=className;
-        var group="#guidecircles";
-        if(myClass=="preview_line"){
-            group="#preview";
-        }  
-        
-		//get circle attributes from click distance & mode
-		if(mode=="circle-center"){
-			var attrs=calculateCircleCenterAttrs();
-		}
-		else if(mode=="circle-edge"){
-			var attrs=calculateCircleEdgeAttrs();
-		}
-        
-		//draw circle
-		$(group).CircleDraw({cx:attrs.cx,cy:attrs.cy,radius:attrs.radius, css_class:className }); 
 
-	}
-	
-	function calculateCircleCenterAttrs(){
-				
-		var x1v=newLineCoord.x1;
-		var y1v=newLineCoord.y1;
-		var x2v=newLineCoord.x2;
-		var y2v=newLineCoord.y2;
-                
-		var magnitude=$("line.preview_line").LineEquation().getMagnitude();
-    
-        //console.log("x1: "+x1v+"y1: "+y1v+" x2: "+x2v+"y:2 "+y2v+" magnatude:"+magnitude);
-        
-		var attrs={
-			cx:x1v,
-			cy:y1v,
-			radius:magnitude
-		}
-		
-		return attrs;
-		
-	}
-	
-	function calculateCircleEdgeAttrs(){
-		var x1v=newLineCoord.x1;
-		var y1v=newLineCoord.y1;
-		var x2v=newLineCoord.x2;
-		var y2v=newLineCoord.y2;
-		var midpoint=$("line.preview_line").LineEquation().findMidpoint();
-		var magnitude=$("line.preview_line").LineEquation().getMagnitude();
-				
-		var attrs={
-			cx:midpoint.x,
-			cy:midpoint.y,
-			radius:magnitude/2
-		}
-		
-		return attrs;
-	}
-	
 	$("#toolbox button").click(function(){
 		$("#toolbox button").removeClass("active");
-		mode=$(this).attr('data-mode');
+		setMode($(this).attr('data-mode'));
 		$(this).addClass("active").blur();
         
         updateNestClass();
         
 	});
+	
+	function setMode(m){
+		mode=m;
+		nodeClicks.setShapeMode(m);
+	}
     
     var updateNestClass =function(){
         $("#nest").removeClass("line").removeClass("erase").removeClass("circle-edge").removeClass("circle-center");
-        
         $("#nest").addClass(mode);
     }    
 	
-	function setUpCanvas(){
-		
-			var frameHeight=Number($('#nest').height());
-			var frameWidth=Number($('#nest').width());
-		
-			$("#guidecircles").CircleDraw();
-			var xCoord=$("circle").attr("cx");
-			var yCoord=$("circle").attr("cy");
-			$("#intersection_points").CircleDraw({cx:xCoord, cy:yCoord, radius:5, css_class:"intersection intersectionPoint"});
-			$("#guidelines").Guideline({x1:xCoord,y1:0,x2:xCoord,y2:frameHeight}).draw();
-			coordDictionary.currentElement=$("line").last(); 
-			coordDictionary.find_coords(); 
-			
-			$("#guidelines").Guideline({x1:0,y1:yCoord,x2:frameWidth,y2:yCoord}).draw();
-			coordDictionary.currentElement=$("line").last();
-			coordDictionary.find_coords();
-            $("#guidelines line").each(function(){
-                var myId=$(this).attr("data-identifier");
-                coordDictionary.removeIntersection(myId);
-            });
-        
+	function  setUpCanvas(){
+			 nodeClicks.newCanvasPolygons();
             windowZoom.updateWindow();
-		
 	}
     
-    function changeCircleMode(){
+    var  changeCircleMode=function(){
         $("#toolbox button").removeClass("active");
         
         if(mode=="circle-edge"){
-            mode="circle-center";   
+            setMode("circle-center");   
             $("#CircleCenterMode").addClass("active");
         } 
         else if(mode=="circle-center"){
-            mode="circle-edge";   
+            setMode("circle-edge");   
             $("#CircleEdgeMode").addClass("active");
         }
         else {
-           mode="circle-center";   
+           setMode("circle-center");   
             $("#CircleCenterMode").addClass("active");
         }    
         
