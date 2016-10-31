6a3a23988f87d4cdae1cebc1e139b32cbf9059b7
MichaelsLevy <michael.levy.vt@gmail.com>
2016-10-30 22:48:07 -0400

    FEAT: add undoHistory.js file

---
+++ js/undo/undoHistory.js
@@ -0,0 +1,18 @@
+var UndoHistory=function(){
+    
+    self=this;
+    
+    var undoHistory=[];
+    self.undoType='';
+    self.undoObjects=[];
+    
+    self.addStep=function(){
+    
+    }
+    
+    self.stepBack=function(){
+    
+    }
+    
+    
+}
