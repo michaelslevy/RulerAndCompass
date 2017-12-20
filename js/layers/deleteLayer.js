42a7574748caf928a5b6622d15114ad50495bb4e
MichaelsLevy <michael.levy.vt@gmail.com>
2017-12-20 12:13:57 -0500

    FEAT: Delete SVG layer and pallette representaion

--- js/layers/deleteLayer.js
+++ js/layers/deleteLayer.js
@@ -5,20 +5,31 @@
         /* Get text of label and insert into confrimation message */
         
         var layerName=$("#PaletteWorkingLayers .heading.selected .layerHeading label").text();
-        
-        console.log(layerName);
-        
+                
         var r=confirm("Are you sure you would like to permanently erase layer: "+layerName);
         
-        console.log(r);
-                
+        if(r==true){
+            deleteCurrentLayer();
+        }
+                        
     });
 
-/* If prompt is confirmed delete layer
+/* If prompt is confirmed delete layer*/
 
-
+var deleteCurrentLayer=function(){
+    
+    /* Retrieve current layer name */
+    var currentLayer=$(".heading.selected").attr("data-identifier");
+    console.log(currentLayer);
+    
     /* Delete layer from palette */
-    
+    $(".layerGroup[data-identifier='"+currentLayer+"']").remove();
+
     /* Delete SVG layer */
+     $("#workingLayers #"+currentLayer).remove();
+
+    /* Create undo feature for restoring layer */        
+
+}
+
     
-    /* Create undo feature for restoring layer */
