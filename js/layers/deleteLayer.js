5ac8e8228544799621189ed3b92837eac437be49
MichaelsLevy <michael.levy.vt@gmail.com>
2017-12-20 16:07:47 -0500

    FEAT: Store deleted layer in undo history

--- js/layers/deleteLayer.js
+++ js/layers/deleteLayer.js
@@ -18,18 +18,29 @@
 
 var deleteCurrentLayer=function(){
     
-    /* Retrieve current layer name */
+     /* Retrieve current layer name */
     var currentLayer=$(".heading.selected").attr("data-identifier");
-    console.log(currentLayer);
     
+    var layer=$("#workingLayers #"+currentLayer).clone().wrap('<div>').parent().html();
+    var pos=$("g.layerGroup").index($("#workingLayers #"+currentLayer));
+    
+    /* Create undo feature for restoring layer */        
+     var undoObj={
+        type:"layerDeletion",
+        content:layer,
+        position: pos
+    }
+
+    var undoHistory=new UndoHistory();
+    undoHistory.addStep(undoObj);
+       
     /* Delete layer from palette */
     $(".layerGroup[data-identifier='"+currentLayer+"']").remove();
 
     /* Delete SVG layer */
      $("#workingLayers #"+currentLayer).remove();
 
-    /* Create undo feature for restoring layer */        
-
+    
 }
 
     
